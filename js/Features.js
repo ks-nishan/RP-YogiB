688bdb61a80aa883638249a22d1c417b66e1d93f
IT20141042 <it20141042@my.sliit.lk>
2023-10-15 09:49:54 +0530

    features updated

---
+++ js/Features.js
@@ -0,0 +1,32 @@
+var swiper = new Swiper(".slide-content", {
+    slidesPerView: 1,
+    spaceBetween: 25,
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    grabCursor: 'true',
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    autoplay: {
+      delay: 2500, // Adjust the delay (in milliseconds) between slides
+      disableOnInteraction: false, // Allow manual navigation to override autoplay
+  },
+    breakpoints:{
+        0: {
+            slidesPerView: 1,
+        },
+        520: {
+            slidesPerView: 1,
+        },
+        950: {
+            slidesPerView: 1,
+        },
+    },
+  });
